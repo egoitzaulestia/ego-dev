1f3e9f381d1ef41072e0b5e36f4e11fe2155b26d
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-30 14:20:01 +0200

    Header component created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import "./Navbar.css";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a class="active" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#news">News</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,16 @@
+import Header from "../Header/Header";
+
 const Home = () => {
   return (
-    <div>
+    <body>
+      <Header />
       <h1>Hello World!</h1>
       <h2>
         I'm Egoitz Aulestia,
         <br />a scientific designer, data scientist and AI developer working in
         complex systems innovation.{" "}
       </h2>
-    </div>
+    </body>
   );
 };
 
