b0318e9d5dbfe4bb11558ad125737da654857dc1
egoitzaulestia <egoitzaulestia@gmail.com>
2025-06-30 14:05:53 +0200

    Home component ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./components/home/home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/home/home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <h2>
+        I'm Egoitz Aulestia,
+        <br />a scientific designer, data scientist and AI developer working in
+        complex systems innovation.{" "}
+      </h2>
+    </div>
+  );
+};
+
+export default Home;
