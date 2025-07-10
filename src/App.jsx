6023d5badfe030115d7b85002f1d0e0cf3fe3dc7
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-10 18:53:11 +0200

    BrowserRouter, Routes and Route established

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
-// import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      {/* <Home /> */}
-      <Contact />
+      <BrowserRouter>
+        <Routes>
+          <Header />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
