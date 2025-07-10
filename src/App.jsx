5f616b6386d311ca9c1eb089fb12aa7ed575d42d
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-10 19:16:30 +0200

    scss update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
